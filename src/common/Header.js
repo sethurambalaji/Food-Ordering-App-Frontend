f0486f9997d47faa5f0612881eba609cce83c085
sethurambalaji <73328800+sethurambalaji@users.noreply.github.com>
2021-07-14 08:14:42 +0530

    Added Header.js and Header.css inside common folder

---
+++ src/common/Header.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+class Header extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div>
+                Header Block
+            </div>
+        )
+    }
+
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Header from './common/Header'
 
 ReactDOM.render(
     <div>
+        <Header/>
         Food Ordering App
     </div>,
     document.getElementById('root')
