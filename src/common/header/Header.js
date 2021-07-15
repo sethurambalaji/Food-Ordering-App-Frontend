9863cf8f4353c2b8ac69a7ef01e5465d2863bd80
sethurambalaji <73328800+sethurambalaji@users.noreply.github.com>
2021-07-15 12:32:11 +0530

    Search Icon and SearchBox inserted and responsive

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,7 +5,15 @@ import AppBar from '@material-ui/core/AppBar';
 import { Toolbar } from '@material-ui/core';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
 import { IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import { InputLabel } from '@material-ui/core';
+import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+
+
 const styles = theme => ({
+
     grow: {
         flexGrow: 1,
     },
@@ -25,34 +33,68 @@ const styles = theme => ({
             flexDirection: 'column',
             alignItems: 'flex-start',
         },
-    
+
     },
-    logoContainer:{},
-    searchBoxContainer:{},
-    loginContainer:{},
+    logoContainer: {},
+    searchBoxContainer: {},
+    searchBox: {
+        [theme.breakpoints.only('xs')]: {
+            marginBottom: theme.spacing(1.5),
+        },
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        width: '30ch',
+    },
+    loginContainer: {},
 
 
 });
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#ffffff',
+        }
+    }
+});
+
+
 
 class Header extends Component {
-    constructor() {
-        super()
-    }
     render() {
         const { classes } = this.props;
+
         return (
             <div className={classes.root}>
                 <AppBar position="static" className={classes.appBar}>
                     <Toolbar className={classes.headerElements}>
                         <div className={classes.logoContainer}>
-                        <IconButton disableRipple={true} edge="start" color="inherit"
-                                    aria-label="app logo">
-                            <FastfoodIcon/>
-                        </IconButton>
+                            <IconButton disableRipple={true} edge="start" color="inherit"
+                                aria-label="app logo">
+                                <FastfoodIcon />
+                            </IconButton>
                         </div>
                         <div className={classes.searchBoxContainer}>
-                            Search box
+                            <div className={classes.searchBox}>
+                                <MuiThemeProvider theme={theme}>
+                                    <InputLabel htmlFor="search-box-input" />
+                                    <Input id="search-box-input"
+                                        startAdornment={
+                                            <InputAdornment position="start">
+                                                <SearchIcon />
+                                            </InputAdornment>
+                                        }
+                                        placeholder="Search by Restaurant Name"
+                                        classes={{
+                                            root: classes.inputRoot,
+                                            input: classes.inputInput,
+                                        }}
+                                    />
+                                </MuiThemeProvider>
+                            </div>
                         </div>
                         <div className={classes.loginContainer}>
                             Login
