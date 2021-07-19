c7de2a77d5232eecd336ee579d9606b1df4953a4
YashwanthAnandS <yashwanthanands@gmail.com>
2021-07-19 19:37:08 +0530

    Changes for serviceWorker and FoodOrder js files.

--- src/FoodOrdering.js
+++ src/FoodOrdering.js
@@ -1,10 +1,13 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 //Router import for redirection.
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 //Imports of different pages in the application
 import Home from './screens/home/Home';
+import Details from "./screens/details/Details";
+
+
 
 /**
  * This class represents the whole FoodOrdering Application.
@@ -18,7 +21,9 @@ class FoodOrdering extends Component {
     render() {
         return (
             <Switch>
-                <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl}/>}/>
+                <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
+                <Route exact path='/restaurant/:restaurantId'
+                       render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
             </Switch>
         )
     }
