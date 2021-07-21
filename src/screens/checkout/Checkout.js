6fe6c814eb4422b69892eb6c277d88699f0d74b7
kavishamodi97 <kavishamodi971@gmail.com>
2021-07-21 18:01:56 +0530

    Created Checkout Page

---
+++ src/screens/checkout/Checkout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Checkout.css';
+
+class Checkout extends Component {
+    render() {
+        return (
+            <div>
+                Checkout Page
+            </div>
+        )
+    }
+}
+
+export default Checkout;
