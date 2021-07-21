8fef91472cfcf23c0d7ea0b77c8a83c0de5d7520
kavishamodi97 <kavishamodi971@gmail.com>
2021-07-21 18:17:05 +0530

    Created Checkout Routing

--- src/FoodOrdering.js
+++ src/FoodOrdering.js
@@ -6,6 +6,7 @@ import { Route, Switch } from "react-router-dom";
 //Imports of different pages in the application
 import Home from './screens/home/Home';
 import Details from "./screens/details/Details";
+import Checkout from "./screens/checkout/Checkout";
 
 
 
@@ -23,7 +24,8 @@ class FoodOrdering extends Component {
             <Switch>
                 <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
                 <Route exact path='/restaurant/:restaurantId'
-                       render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
+                    render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
+                <Route exact path='/checkout' render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
             </Switch>
         )
     }
